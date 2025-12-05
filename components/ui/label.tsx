95cd44bf2d8374180c37a97c4893d8662d48bfa7
salesplatform-org <andresmhernan@icloud.com>
2025-12-04 19:42:06 -0700

    Update label.tsx

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -6,10 +6,7 @@ const Label = React.forwardRef<HTMLLabelElement, React.LabelHTMLAttributes<HTMLL
   ({ className, ...props }, ref) => (
     <label
       ref={ref}
-      className={cn(
-        "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
-        className
-      )}
+      className={cn("text-sm font-medium leading-none", className)}
       {...props}
     />
   )
