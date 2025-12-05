26c9e289f66b6fd7c268772a858799e17cd7f62c
salesplatform-org <andresmhernan@icloud.com>
2025-12-04 19:42:42 -0700

    Create utils.ts

---
+++ lib/utils.ts
@@ -0,0 +1,7 @@
+// path: lib/utils.ts (create this file if missing)
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
