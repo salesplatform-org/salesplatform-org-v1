7c1a47e09b90c4a953a56635f745d249825e8d54
salesplatform-org <andresmhernan@icloud.com>
2025-12-06 15:34:48 -0700

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,4 +1,7 @@
 // path: lib/supabase/client.ts
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { createClient } from "@supabase/ssr";
 
-export const createClient = () => createBrowserSupabaseClient();
+export const createClient = () => createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
