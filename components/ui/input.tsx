a7dabf8e2a9cc37da37d82ec389d449fa2e862a1
salesplatform-org <andresmhernan@icloud.com>
2025-12-06 15:59:09 -0700

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,16 +1,11 @@
-import * as React from "react";
 import { cn } from "@/lib/utils";
 
-export const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
-  ({ className, ...props }, ref) => (
-    <input
-      className={cn(
-        "flex h-11 w-full rounded-md border border-white/10 bg-black px-3 py-2 text-sm placeholder:text-white/40 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-white/20",
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  )
+export const Input = ({ className, ...props }) => (
+  <input
+    className={cn(
+      "flex h-11 w-full rounded-md border border-white/10 bg-black px-3 py-2 text-sm placeholder:text-white/40 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-white/20",
+      className
+    )}
+    {...props}
+  />
 );
-Input.displayName = "Input";
